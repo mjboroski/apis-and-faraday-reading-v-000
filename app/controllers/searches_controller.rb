1ca325408b4f5e03929aec74347176aef95f3e6b
Michael Boroski <mjboroski@gmail.com>
2018-07-11 03:07:14 +0000

    automatically backed up by learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -9,7 +9,6 @@ class SearchesController < ApplicationController
       req.params['v'] = '20160201'
       req.params['near'] = params[:zipcode]
       req.params['query'] = 'coffee shop'
-      req.options.timeout = 0
     end
     render 'search'
   end
